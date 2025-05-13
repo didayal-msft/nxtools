9b430d6cb0a2c28f9a41a753c40c0cc6338eba8b
Divyadeep Dayal <didayal@microsoft.com>
2025-05-13 19:57:27 +0530

    added bash support

--- source/Classes/1.DscResources/08.nxScript.ps1
+++ source/Classes/1.DscResources/08.nxScript.ps1
@@ -1,6 +1,9 @@
 [DscResource()]
-class nxScript
-{
+class nxScript {
+    [DscProperty()]
+    [ValidateSet("PowerShell", "Bash")]
+    [System.String] $ScriptType = "PowerShell"
+
     [DscProperty()]
     [System.String] $GetScript
 
@@ -13,72 +16,98 @@ class nxScript
     [DscProperty()]
     [Reason[]] $Reasons
 
-    [nxScript] Get()
-    {
-        if ([string]::IsNullOrEmpty($this.GetScript))
-        {
+    [nxScript] Get() {
+        if ([string]::IsNullOrEmpty($this.GetScript)) {
             $Reason = [Reason]::new()
             $Reason.Code = "Script:Script:GetScriptNotDefined"
             $Reason.Phrase = "Cannot determine reason for non-compliance. Please define Reasons in the hashtable returned from the GetScript script block to return reasons for non-compliance."
             $this.Reasons = @($Reason)
             return $this
         }
+        if ($this.ScriptType -eq 'PowerShell') {
+            $scriptBlock = [System.Management.Automation.ScriptBlock]::Create($this.GetScript)
+            $invokeScriptResult = $this.InvokeScript($scriptBlock)
+            if ($invokeScriptResult -is [System.Management.Automation.ErrorRecord]) {
+                throw "The GetScript script block returned an error: $invokeScriptResult."
+            }
 
-        $scriptBlock = [System.Management.Automation.ScriptBlock]::Create($this.GetScript)
-        $invokeScriptResult = $this.InvokeScript($scriptBlock)
-        if ($invokeScriptResult -is [System.Management.Automation.ErrorRecord])
-        {
-            throw "The GetScript script block returned an error: $invokeScriptResult."
+            $isValid = $this.TestGetScriptOutput($invokeScriptResult)
+            if (-not $isValid) {
+                throw "The GetScript script block must return a hashtable that contains a non-empty list of Reason objects under the Reasons key."
+            }
+
+            $this.Reasons = $invokeScriptResult.Reasons
+            return $this
         }
-
-        $isValid = $this.TestGetScriptOutput($invokeScriptResult)
-        if (-not $isValid)
-        {
-            throw "The GetScript script block must return a hashtable that contains a non-empty list of Reason objects under the Reasons key."
+        elseif ($this.ScriptType -eq 'Bash') {
+            $CurrentState = [nxScript]::new()
+            #write-verbose "executing the get script"
+            $rnd = Get-Random -Minimum 100 -Maximum 999
+            "$($this.getscript)" | out-file "\tmp\getscript$rnd$(".sh")" -force
+            Set-Location \tmp
+            $res = Invoke-Expression "sudo sh getscript$rnd.sh" | out-null 
+            Remove-Item "\tmp\getscript$rnd$(".sh")" -force
+            $CurrentState.ConfigurationScope = $this.ConfigurationScope
+            $this.CachedCurrentState = $CurrentState
+            return $CurrentState
         }
-
-        $this.Reasons = $invokeScriptResult.Reasons
-        return $this
     }
 
-    [bool] Test()
-    {
-        $scriptBlock = [System.Management.Automation.ScriptBlock]::Create($this.TestScript)
-        $invokeScriptResult = $this.InvokeScript($scriptBlock)
-        if ($invokeScriptResult -is [System.Management.Automation.ErrorRecord])
-        {
-            throw "The TestScript script block returned an error: $invokeScriptResult."
-        }
+    [bool] Test() {
+        if ($this.ScriptType -eq 'PowerShell') {
+            $scriptBlock = [System.Management.Automation.ScriptBlock]::Create($this.TestScript)
+            $invokeScriptResult = $this.InvokeScript($scriptBlock)
+            if ($invokeScriptResult -is [System.Management.Automation.ErrorRecord]) {
+                throw "The TestScript script block returned an error: $invokeScriptResult."
+            }
 
-        if ($null -eq $invokeScriptResult -or -not ($invokeScriptResult -is [System.Boolean]))
-        {
-            throw "The TestScript script block must return a Boolean."
-        }
+            if ($null -eq $invokeScriptResult -or -not ($invokeScriptResult -is [System.Boolean])) {
+                throw "The TestScript script block must return a Boolean."
+            }
 
-        return $invokeScriptResult
+            return $invokeScriptResult
+        }
+        elseif ($this.ScriptType -eq 'Bash') {
+            $rnd = Get-Random -Minimum 100 -Maximum 999
+            "$($this.testscript)" | out-file "\tmp\testscript$rnd$(".sh")" -force
+            Set-Location \tmp
+            [int]$res = Invoke-Expression "sudo sh testscript$rnd.sh"
+            Remove-Item "\tmp\testscript$rnd$(".sh")" -force
+            return $res
+        }
+        else {
+            throw "Unsupported script type: $($this.ScriptType). Supported types are 'PowerShell' and 'Bash'."
+        }
     }
 
-    [void] Set()
-    {
-        if ([string]::IsNullOrEmpty($this.SetScript))
-        {
+    [void] Set() {
+        if ([string]::IsNullOrEmpty($this.SetScript)) {
             # The SetScript script block was not defined
             return
         }
-
-        $scriptBlock = [System.Management.Automation.ScriptBlock]::Create($this.SetScript)
-        $null = $this.InvokeScript($scriptBlock)
+        if ($this.ScriptType -eq 'PowerShell') {
+            $scriptBlock = [System.Management.Automation.ScriptBlock]::Create($this.SetScript)
+            $null = $this.InvokeScript($scriptBlock)
+        }
+        elseif ($this.ScriptType -eq 'Bash') {
+            $rnd = Get-Random -Minimum 100 -Maximum 999
+            "$($this.SetScript)" | out-file "\tmp\setscript$rnd$(".sh")" -force
+            Set-Location \tmp
+            [int]$res = Invoke-Expression "sudo sh setscript$rnd.sh" | out-null
+            Remove-Item "\tmp\setscript$rnd$(".sh")" -force
+        }
+        else {
+            throw "Unsupported script type: $($this.ScriptType). Supported types are 'PowerShell' and 'Bash'."
+        }
+        
     }
 
-    [System.Object] InvokeScript([System.Management.Automation.ScriptBlock] $ScriptBlock)
-    {
+    [System.Object] InvokeScript([System.Management.Automation.ScriptBlock] $ScriptBlock) {
         $scriptResult = $null
-        try
-        {
+        try {
             $scriptResult = & $ScriptBlock
         }
-        catch
-        {
+        catch {
             # Surfacing the error thrown by the execution of the script
             $scriptResult = $_
         }
@@ -86,31 +115,25 @@ class nxScript
         return $scriptResult
     }
 
-    [bool] TestGetScriptOutput([System.Object] $GetScriptOutput)
-    {
-        if ($GetScriptOutput -isnot [System.Collections.Hashtable])
-        {
+    [bool] TestGetScriptOutput([System.Object] $GetScriptOutput) {
+        if ($GetScriptOutput -isnot [System.Collections.Hashtable]) {
             Write-Verbose -Message "The GetScript script block must return a hashtable"
             return $false
         }
 
-        if (-not $GetScriptOutput.ContainsKey("Reasons"))
-        {
+        if (-not $GetScriptOutput.ContainsKey("Reasons")) {
             Write-Verbose -Message "The hashtable returned by GetScript does not contain a Reasons key"
             return $false
         }
 
         $outputReasons = $GetScriptOutput["Reasons"]
-        if ($outputReasons.Count -eq 0)
-        {
+        if ($outputReasons.Count -eq 0) {
             Write-Verbose -Message "The Reasons list returned by GetScript is empty"
             return $false
         }
 
-        foreach ($outputReason in $outputReasons)
-        {
-            if ($outputReason -isnot [Reason])
-            {
+        foreach ($outputReason in $outputReasons) {
+            if ($outputReason -isnot [Reason]) {
                 Write-Verbose -Message "One of the Reasons returned by GetScript is not a Reason object"
                 return $false
             }
